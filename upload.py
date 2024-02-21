62b905343ca59dd6b84fd86eeaec1c3529cc57c0
Jaeheuk Jung <jung.j.4@Jaeheukui-iMac.local>
2024-02-21 16:48:26 +0900

    finished the uploading process

--- upload.py
+++ upload.py
@@ -2,20 +2,29 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql+psycopg2://sofsys:withus4u!@sofsys.postgres.database.azure.com/postgres',connect_args={'sslmode': 'require'})
 
 
 
 class upload_data:
     def __init__(self):
-        current_directory = os.path.dirname(os.path.abspath(__file__))
-        self.data_file = [f for f in os.listdir(current_directory) if f.endswith('.csv')]
+        self.current_directory = os.path.dirname(os.path.abspath(__file__))
+        self.data_file = [f for f in os.listdir(self.current_directory) if f.endswith('.csv')]
+        self.engine = create_engine('postgresql+psycopg2://sofsys:withus4u!@sofsys.postgres.database.azure.com/postgres',connect_args={'sslmode': 'require'})
+
 
     def take_data(self):
         data = self.data_file[0]
         df = pd.DataFrame(pd.read_csv(data))
-        print(len(df.columns))
+
+        try :
+            df.to_sql('naver_smart_store_stats', self.engine, if_exists='append', index=False)
         
+        except Exception as e:
+            print(e)
+        else: 
+            file = [f for f in os.listdir(self.current_directory) if f.endswith('.csv')][0]
+            os.remove(file)
+
 
 
 if __name__ == '__main__':
