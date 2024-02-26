1023742bd7ccfd24299b6a3633e2184870f58c49
Jae won Jung <jaewonjung@Jaes-MacBook-Pro.local>
2024-02-26 10:37:45 +0900

    finished with updating

--- login.py
+++ login.py
@@ -33,9 +33,6 @@ class NaverLogin:
         url = 'https://sell.smartstore.naver.com/#/home/about'
 
         driver.get(url)
-                
-
-
 
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/ui-view[1]/div[2]/div[2]/div/div[1]/div[2]/button[1]"))) # login button
         driver.find_element(By.XPATH, "/html/body/ui-view[1]/div[2]/div[2]/div/div[1]/div[2]/button[1]").click()
@@ -63,21 +60,28 @@ class NaverLogin:
         driver.find_element(By.CLASS_NAME, "btn_login").click() # login button
 
         driver.switch_to.window(driver.window_handles[0]) # switch back to original window
-        time.sleep(1)
-
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/a'))) # click on statistics
-        driver.find_element(By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/a').click()
-
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/ul/li[7]/a'))) # click on 고객현황
-        driver.find_element(By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/ul/li[7]/a').click()
         
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-content"]/div/div/div/div[1]/div[1]/button'))) # wait till close button appears
-        driver.find_element(By.XPATH, '//*[@id="seller-content"]/div/div/div/div[1]/div[1]/button').click() 
-        
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-content"]/ui-view/div[2]/div[2]/div[2]/div[1]/div[2]/div/div/button'))) # click on 다운로드
-        driver.find_element(By.XPATH, '//*[@id="seller-content"]/ui-view/div[2]/div[2]/div[2]/div[1]/div[2]/div/div/button').click()
+        for i in range (0, 10):
+            while True:
+                try:
+                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/a'))) # click on statistics
+                    driver.find_element(By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/a').click()
 
-        time.sleep(5)
+                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/ul/li[7]/a'))) # click on 고객현황
+                    driver.find_element(By.XPATH, '//*[@id="seller-lnb"]/div/div[1]/ul/li[14]/ul/li[7]/a').click()
+                    
+                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="seller-content"]/ui-view/div[2]/div[2]/div[2]/div[1]/div[2]/div/div/button'))) # click on 다운로드
+                    driver.find_element(By.XPATH, '//*[@id="seller-content"]/ui-view/div[2]/div[2]/div[2]/div[1]/div[2]/div/div/button').click()
+                    break
+                except Exception as e:
+                    print(e)
+                    print('retrying!')
+                    WebDriverWait(driver, 50).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="seller-content"]/div/div/div/div[1]/div[1]/button'))) # wait till close button appears
+                    driver.find_element(By.XPATH, '//*[@id="seller-content"]/div/div/div/div[1]/div[1]/button').click()
+                    continue
+
+        
+
 
         driver.quit()
 
--- upload.py
+++ upload.py
@@ -3,29 +3,27 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-
-
 class upload_data:
     def __init__(self):
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
         self.data_file = [f for f in os.listdir(self.current_directory) if f.endswith('.csv')]
         self.engine = create_engine('postgresql+psycopg2://sofsys:withus4u!@sofsys.postgres.database.azure.com/postgres',connect_args={'sslmode': 'require'})
 
-
     def take_data(self):
+        connection = self.engine.raw_connection()
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM naver_smart_store_stats WHERE id NOT IN (SELECT MIN(id) FROM naver_smart_store_stats GROUP BY 기준일);')
+        
         data = self.data_file[0]
         df = pd.DataFrame(pd.read_csv(data))
 
         try :
-            df.to_sql('naver_smart_store_stats', self.engine, if_exists='append', index=False)
-        
+            df.to_sql('naver_smart_store_stats', self.engine, if_exists='append', index=False, index_label='기준일')
         except Exception as e:
             print(e)
         else: 
             file = [f for f in os.listdir(self.current_directory) if f.endswith('.csv')][0]
             os.remove(file)
-
-
 
 if __name__ == '__main__':
     upload = upload_data()
